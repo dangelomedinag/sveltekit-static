6fafa7e65f0af6d53e4ca26e24c1f07d40a3ed76
dangelomedinag <dangelomedinag@gmail.com>
2024-03-21 15:12:56 -0300

    add client interactive in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
+<script>
+	import { quintOut } from 'svelte/easing';
+	import { scale } from 'svelte/transition';
+
+	let count = 5;
+</script>
+
+<button on:click={() => count++} disabled={count > 10}>{count}</button>
+{#if count > 10}
+	<button on:click={() => (count = 5)}>reset</button>
+{/if}
+<header>
+	{#key count}
+		<img
+			in:scale={{ easing: quintOut }}
+			src="./favicon.png"
+			alt="logo"
+			style:--size-logo={`${count * 20}px`}
+		/>
+	{/key}
+</header>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	img {
+		width: var(--size-logo);
+	}
+</style>
